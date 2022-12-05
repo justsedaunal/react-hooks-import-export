3ea3969ac3c3017d72cdcc6dcd7b2319239bd369
Seda Ünal <sedaylmz06@outlook.com>
2022-12-05 20:50:30 +0300

    import export

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,27 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {
+  trees as parkTrees,
+  wildlife as parkWildlife,
+} from "./parks/RockyMountain";
+
+console.log(parkTrees);
+// > "Aspen and Pine"
+
+parkWildlife();
+// > "Elk, Bighorn Sheep, Moose"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <>
+      <div>
+        <MesaVerde />
+      </div>
+    </>
+  );
 }
+
+export default ColoradoStateParks;
